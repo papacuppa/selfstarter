c1273af41ecb1bc793dc8cf5148b41269ff44c96
papacuppa <david.hawkins87@gmail.com>
2013-01-25 21:00:50 +0000

    change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
- # Displays a textual representation of +number+ in british pound  
-format (i.e.:
-  # "£1,359.56").
-  def number_to_currency_gbp (number)
-    number_to_currency(number, { :unit => "&pound;"})
-  end
+def number_to_currency(number, options = {})
+options = options.stringify_keys
+precision, unit, separator, delimiter = options.delete("precision") { 2
+}, options.delete("unit") { "£" }, options.delete("separator") { "." },
+options.delete("delimiter") { "," }
+separator = "" unless precision > 0
+begin
+	parts = number_with_precision(number, precision).split('.')
+	unit + number_with_delimiter(parts[0], delimiter) + separator +
+parts[1].to_s
+rescue
+	number
 end
