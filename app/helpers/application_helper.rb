cf42db936aeee12f4930250cf5cd6162fa7ebd49
papacuppa <david.hawkins87@gmail.com>
2013-01-25 20:57:53 +0000

    change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+ # Displays a textual representation of +number+ in british pound  
+format (i.e.:
+  # "£1,359.56").
+  def number_to_currency_gbp (number)
+    number_to_currency(number, { :unit => "&pound;"})
+  end
 end
