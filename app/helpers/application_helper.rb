266f9579d5b37c68a4b6770c00c5198cedb9865c
papacuppa <david.hawkins87@gmail.com>
2013-01-25 21:03:27 +0000

    change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,3 @@
 module ApplicationHelper
-def number_to_currency(number, options = {})
-options = options.stringify_keys
-precision, unit, separator, delimiter = options.delete("precision") { 2
-}, options.delete("unit") { "£" }, options.delete("separator") { "." },
-options.delete("delimiter") { "," }
-separator = "" unless precision > 0
-begin
-	parts = number_with_precision(number, precision).split('.')
-	unit + number_with_delimiter(parts[0], delimiter) + separator +
-parts[1].to_s
-rescue
-	number
+
 end
