d3c0f4ad0ce3ef50dfc99e5c5bd15050d3c7ae33
papacuppa <david.hawkins87@gmail.com>
2013-02-04 22:54:46 +0000

    change

--- config/initializers/gocardless.rb
+++ config/initializers/gocardless.rb
@@ -1,7 +1,8 @@
 GoCardless.account_details = {
-GoCardless.environment = :sandbox 
   app_id: Settings.app_id,
   app_secret: Settings.app_secret,
   token: Settings.token,
   merchant_id: Settings.merchant_id
 }
+
+GoCardless.environment = :sandbox unless Rails.env.production?
