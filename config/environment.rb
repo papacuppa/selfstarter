e023c66139640f059f3a18319e4384040fb42056
papacuppa <david.hawkins87@gmail.com>
2013-02-05 23:02:50 +0000

    change

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
-# -*- encoding : utf-8 -*-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Selfstarter::Application.initialize!
-
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
